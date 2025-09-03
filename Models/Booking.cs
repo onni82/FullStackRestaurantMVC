c02571ab55ad0aa6e51a11644f8765e2c45e588b
onni82 <onni.bucht@me.com>
2025-09-03 10:49:25 +0200

    Updated models

--- Models/Booking.cs
+++ Models/Booking.cs
@@ -2,5 +2,11 @@
 {
     public class Booking
     {
+        public int Id { get; set; }
+        public DateTime Start {  get; set; }
+        public DateTime End { get; set; }
+        public int Guests { get; set; }
+        public int TableId { get; set; }
+        public int CustomerId { get; set; }
     }
 }
--- Models/Table.cs
+++ Models/Table.cs
@@ -2,5 +2,8 @@
 {
     public class Table
     {
+        public int Id { get; set; }
+        public int TableNumber { get; set; }
+        public int Seats { get; set; }
     }
 }
