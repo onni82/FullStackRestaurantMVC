d1c0374637f6a68253447da18be06179746bb41d
onni82 <onni.bucht@me.com>
2025-09-05 11:32:17 +0200

    Implemented auth methods

--- Controllers/AuthController.cs
+++ Controllers/AuthController.cs
@@ -1,12 +1,46 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using FullStackRestaurantMVC.Services;
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Json;
 
 namespace FullStackRestaurantMVC.Controllers
 {
     public class AuthController : Controller
     {
-        public IActionResult Index()
+        private readonly ApiService _apiService;
+
+        public AuthController(ApiService apiService)
         {
-            return View();
+            _apiService = apiService;
+        }
+
+        [HttpGet]
+        public IActionResult Login() => View();
+
+        [HttpPost]
+        public async Task<IActionResult> Login(string username, string password)
+        {
+            var res = await _apiService.PostAsync<JsonElement>("api/Auth/login", new { username, password });
+
+            if (res.ValueKind == JsonValueKind.Undefined)
+            {
+                ViewBag.Error = "Felaktiga inloggningsuppgifter";
+                return View();
+            }
+
+            var token = res.GetProperty("token").GetString();
+            _apiService.SetToken(token);
+
+            // Spara token i session för framtida anrop
+            HttpContext.Session.SetString("JwtToken", token);
+
+            return RedirectToAction("Index", "Home");
+        }
+
+        public IActionResult Logout()
+        {
+            _apiService.SetToken(null);
+            HttpContext.Session.Remove("JwtToken");
+            return RedirectToAction("Index", "Home");
         }
     }
 }
