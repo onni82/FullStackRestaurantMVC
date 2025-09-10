f6f43659bba0bef6feb3c624e4da7fca00013cc8
onni82 <onni.bucht@me.com>
2025-09-10 14:41:54 +0200

    Added MenuController again

---
+++ Controllers/MenuController.cs
@@ -0,0 +1,22 @@
+﻿using FullStackRestaurantMVC.Models;
+using FullStackRestaurantMVC.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FullStackRestaurantMVC.Controllers
+{
+    public class MenuController : Controller
+    {
+        private readonly ApiService _apiService;
+
+        public MenuController(ApiService apiService)
+        {
+            _apiService = apiService;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var items = await _apiService.GetAsync<IEnumerable<MenuItem>>("api/MenuItems");
+            return View(items ?? new List<MenuItem>());
+        }
+    }
+}
