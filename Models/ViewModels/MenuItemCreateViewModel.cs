5e2eea32e0c650279274dd4d7462d21eae166d1e
onni82 <onni.bucht@me.com>
2025-09-11 14:03:32 +0200

    Added MenuItemCreateViewModel

---
+++ Models/ViewModels/MenuItemCreateViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FullStackRestaurantMVC.Models.ViewModels
+{
+    public class MenuItemCreateViewModel
+    {
+        [Required]
+        [StringLength(200)]
+        public string Name { get; set; } = string.Empty;
+
+        [Required]
+        [Range(0.01, 9999.99)]
+        public decimal Price { get; set; }
+
+        [StringLength(4000)]
+        public string? Description { get; set; }
+
+        public bool IsPopular { get; set; }
+
+        [Url]
+        [StringLength(500)]
+        public string? ImageUrl { get; set; }
+    }
+}
